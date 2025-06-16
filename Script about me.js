966618ba9be73ba5237282f1fdc22ca954b53041
MediciJor <cgbear360@gmail.com>
2025-06-16 11:14:46 +0800

    fixed???

--- Script about me.js
+++ Script about me.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  fetch('about-text.html')
+  fetch('About me.html')
     .then(response => {
       if (!response.ok) {
         throw new Error("Failed to load about text");
